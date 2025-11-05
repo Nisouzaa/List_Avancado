e4db02b7dd22c7e83f78d212c3d57400ebc09051
Nisouzaa <souzanini@gmail.com>
2025-11-05 17:27:56 -0300

    atualização Pj

--- Js/script.js
+++ Js/script.js
@@ -1,5 +1,51 @@
-const text = document.querySelector("h1");
+// Seleção de eventos
+const todoForm = document.querySelector("#todo-form");
+const todoInput = document.querySelector("#todo-input");
+const todoList = document.querySelector("#todo-list");
+const editForm = document.querySelector("#edit-form");
+const editInput = document.querySelector("#edit-input");
+const cancelEditBtn = document.querySelector("#cancel-edit-btn");
 
-text.addEventListener("click", () => {
-  console.log("Clicou no Texto");
+// Funções
+const saveTodo = (text) => {
+  const todo = document.createElement("div");
+  todo.classList.add("todo");
+
+  const todoTitle = document.createElement("h3");
+  todoTitle.innerText = text;
+  todo.appendChild(todoTitle);
+
+  // 1º Btn
+  const doneBtn = document.createElement("button");
+  doneBtn.classList.add("finish-todo");
+  doneBtn.innerHTML = ` <i class="fa-solid fa-check"></i>`;
+  todo.appendChild(doneBtn);
+
+  // 2º Btn
+  const editBtn = document.createElement("button");
+  editBtn.classList.add("edit-todo");
+  editBtn.innerHTML = ` <i class="fa-solid fa-pen"></i>`;
+  todo.appendChild(editBtn);
+
+  // 3º Btn
+  const deleteBtn = document.createElement("button");
+  deleteBtn.classList.add("remove-todo");
+  deleteBtn.innerHTML = ` <i class="fa-solid fa-xmark"></i>`;
+  todo.appendChild(deleteBtn);
+
+  todoList.appendChild(todo);
+
+  todoInput.value = "";
+  todoInput.focus();
+};
+
+// Evenetos
+todoForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const inputValue = todoInput.value;
+
+  if (inputValue) {
+    saveTodo(inputValue);
+  }
 });
